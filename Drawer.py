9f558eaea00f940f2b30120a6edd2041296f7d3e
Leo Liu <leo@venske.net>
2019-09-25 01:33:50 +1200

    Bad Smell: Speculative Generality

--- Drawer.py
+++ Drawer.py
@@ -6,18 +6,18 @@ class Drawer(object):
         self.canvas = canvas
         # self.selectPen(0)
         # self.penUp()
-
-    def selectPen(self,penNum):
-        print("Selected pen " + str(penNum))
-
-    def penDown(self):
-        print("pen down")
-
-    def penUp(self):
-        print("pen up")
-
-    def drawLine(self, direction, distance):
-        print("drawing line of length " + str(distance) + " at " + str(direction))
-
-    def drawCircle(self, radius):
-        print("drawing circle of radius " + str(radius))
+    #
+    # def selectPen(self,penNum):
+    #     print("Selected pen " + str(penNum))
+    #
+    # def penDown(self):
+    #     print("pen down")
+    #
+    # def penUp(self):
+    #     print("pen up")
+    #
+    # def drawLine(self, direction, distance):
+    #     print("drawing line of length " + str(distance) + " at " + str(direction))
+    #
+    # def drawCircle(self, radius):
+    #     print("drawing circle of radius " + str(radius))
